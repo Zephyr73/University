972e70b4d8f31860dd6eb36d1917f59b91bcd4df
unknown <tasrif73@gmail.com>
2026-03-11 12:59:50 -0400

    Auto-sync from laptop: 11-Mar-26 12:59:50.51

---
+++ LaGuardia Community College/MAC 286/Classwork/Class 2/Project 2/src/LowArray.java
@@ -0,0 +1,14 @@
+class LowArray
+{
+    private long[] a;                 // ref to array a
+    //--------------------------------------------------------------
+    public LowArray(int size)         // constructor
+    { a = new long[size]; }        // create array
+    //--------------------------------------------------------------
+    public void setElem(int index, long value)     // set value
+    { a[index] = value; }
+    //--------------------------------------------------------------
+    public long getElem(int index)              // get value
+    { return a[index]; }
+//--------------------------------------------------------------
+}  // end class LowArray
